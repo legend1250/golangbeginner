6549bac8adb39557d1f2bda11c48dbd64130a58f
Vinh <vinh_nguyen1211@yahoo.com.vn>
2017-12-12 15:24:27 +0700

    inital commit

---
+++ src/hello/hello.go
@@ -0,0 +1,46 @@
+package main
+
+import "fmt"
+//import "math"
+
+const s string = "constant"
+
+func main(){
+	// <<<<<<<>>>>>>>>	Lession 1: Variable
+	//var a string = "hello";
+
+	// var c int = 10;
+
+	// fmt.Println("c +10: ", c+10);
+
+	// var t = true;
+	// fmt.Println(t);
+
+	// <<<<<<<>>>>>>>>	Lession 2: Variable continute
+	// const n = 500000000;
+	// const d = 3e20/n;
+	// fmt.Println("d before: " ,d);
+	// fmt.Println("d After : " ,(int64 (d)))
+
+	// fmt.Println("sin: ", math.Sin(n))
+
+
+	// <<<<<<<>>>>>>>> Lession 3: Loop
+	// for i:=1 ; i <= 10; i++{
+		
+	// 	if( i%2 == 0){
+	// 		continue
+	// 	}
+	// 	fmt.Println(i)
+	// }
+	
+
+	// <<<<<<<>>>>>>>> Lession 4: If/else
+	
+	if(3%2 == 0){
+		fmt.Println("3 is even")
+	} else {
+		fmt.Println("3 is odd")
+	}
+
+}
