cf29835a0a18e34f2cea265e0b1dd8aea32e2fca
Vinh <vinh_nguyen1211@yahoo.com.vn>
2017-12-13 16:01:34 +0700

    update from laptop

--- src/hello/hello.go
+++ src/hello/hello.go
@@ -2,6 +2,8 @@ package main
 
 import "fmt"
 //import "math"
+// import "time"
+
 
 const s string = "constant"
 
@@ -37,10 +39,63 @@ func main(){
 
 	// <<<<<<<>>>>>>>> Lession 4: If/else
 	
-	if(3%2 == 0){
-		fmt.Println("3 is even")
-	} else {
-		fmt.Println("3 is odd")
-	}
+	// if(3%2 == 0){
+	// 	fmt.Println("3 is even")
+	// } else {
+	// 	fmt.Println("3 is odd")
+	// }
 
+
+	// <<<<<<<>>>>>>>> Lession 5: Switch
+	// i := 2;
+
+	// fmt.Println("Write i: ", i, " as ");
+
+	// switch i {
+	// case 1:
+	// 	fmt.Println("Hello one")
+	// case 2:
+	// 	fmt.Println("Hello two")
+	// }
+
+	// t := time.Now().Day();
+	// fmt.Println("Now is: ", t)
+
+	// whatAmI := func(i interface{}){
+	// 	switch t := i.(type) {
+	// 	case bool:
+	// 		fmt.Println("I'm a Boolean")
+	// 	case int:
+	// 		fmt.Println("I'm an Integer")
+	// 	default: 
+	// 		fmt.Println("Don't know type %T\n", t)		
+	// 	}
+		
+	// }
+
+	// whatAmI("true")
+
+
+	// <<<<<<<>>>>>>>> Lession: slice
+	s := make([] string, 3)
+	fmt.Println("emp:", s)
+	s[0] = "a"
+	s[1] = "b"
+	s[2] = "c"
+	s = append(s , "e", "f")
+	fmt.Println(s)
+	fmt.Println("length current: ", len(s))
+
+	c := make([] string, len(s))
+	copy(c,s)
+	fmt.Println("c: " ,c)
+	fmt.Println("-----------------Slice-----------")
+	l := s[2:5]
+	fmt.Println("l: " ,l)
+
+	l = s[:5]
+	fmt.Println("sl2:" ,l)
+	l = s[2:]
+	fmt.Println("sl3:", l)
+	
 }
